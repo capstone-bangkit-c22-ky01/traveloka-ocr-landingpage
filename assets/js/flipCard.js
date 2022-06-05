704bc8979bd1a42cfbf040660b833b97403f0fc1
Faisal <mfaisalhidayatullah@gmail.com>
2022-06-05 16:29:19 +0700

    add profile cards

---
+++ assets/js/flipCard.js
@@ -0,0 +1,90 @@
+$('#card1').flip({
+	trigger: 'manual',
+});
+
+$('#flip-btn1').click(function () {
+	$('#card1').flip(true);
+});
+
+$('#unflip-btn1').click(function () {
+	$('#card1').flip(false);
+});
+$('#card2').flip({
+	trigger: 'manual',
+});
+$('#flip-btn2').click(function () {
+	$('#card2').flip(true);
+});
+$('#unflip-btn2').click(function () {
+	$('#card2').flip(false);
+});
+
+$('#card3').flip({
+	trigger: 'manual',
+});
+$('#flip-btn3').click(function () {
+	$('#card3').flip(true);
+});
+$('#unflip-btn3').click(function () {
+	$('#card3').flip(false);
+});
+
+$('#card4').flip({
+	trigger: 'manual',
+});
+$('#flip-btn4').click(function () {
+	$('#card4').flip(true);
+});
+$('#unflip-btn4').click(function () {
+	$('#card4').flip(false);
+});
+
+$('#card5').flip({
+	trigger: 'manual',
+});
+$('#flip-btn5').click(function () {
+	$('#card5').flip(true);
+});
+$('#unflip-btn5').click(function () {
+	$('#card5').flip(false);
+});
+
+$('#card6').flip({
+	trigger: 'manual',
+});
+$('#flip-btn6').click(function () {
+	$('#card6').flip(true);
+});
+$('#unflip-btn6').click(function () {
+	$('#card6').flip(false);
+});
+
+$('#card7').flip({
+	trigger: 'manual',
+});
+$('#flip-btn7').click(function () {
+	$('#card7').flip(true);
+});
+$('#unflip-btn7').click(function () {
+	$('#card7').flip(false);
+});
+
+$('#card8').flip({
+	trigger: 'manual',
+});
+$('#flip-btn8').click(function () {
+	$('#card8').flip(true);
+});
+$('#unflip-btn8').click(function () {
+	$('#card8').flip(false);
+});
+
+$('#card9').flip({
+	trigger: 'manual',
+});
+$('#flip-btn9').click(function () {
+	$('#card9').flip(true);
+});
+$('#unflip-btn9').click(function () {
+	$('#card9').flip(false);
+});
